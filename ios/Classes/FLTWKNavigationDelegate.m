d1f4b43b27ed2df41768038e2d6b8a6da9730f6b
Ant <weibintao@orange1.com.cn>
2020-11-13 17:30:21 +0800

    no message

--- ios/Classes/FLTWKNavigationDelegate.m
+++ ios/Classes/FLTWKNavigationDelegate.m
@@ -30,28 +30,37 @@
     NSString *reqUrl = url.absoluteString;
 //    NSLog(@"-url---- %@",reqUrl);
     NSString *scheme = [url scheme];
-    NSString *CompanyFirstDomainByWeChat = @"www.haodf.com";
 
     static NSString *endPayRedirectURL = nil;
-        // H5微信支付完,跳回到APP
-        if ([reqUrl hasPrefix:@"https://wx.tenpay.com/cgi-bin/mmpayweb-bin/checkmweb"] && ![reqUrl hasSuffix:[NSString stringWithFormat:@"redirect_url=%@://",CompanyFirstDomainByWeChat]]) {
-            decisionHandler(WKNavigationActionPolicyCancel);
+    // H5微信支付完,跳回到APP
+    NSString _attachUrl = @"";
+    if ([_urlStr containsString:@"nidianme.com"]) {
+        _attachUrl = @"nidianme.com";
+    }
 
-            NSString *redirectUrl = nil;
-            if ([reqUrl containsString:@"redirect_url="]) {
-                NSRange redirectRange = [reqUrl rangeOfString:@"redirect_url"];
-                endPayRedirectURL =  [reqUrl substringFromIndex:redirectRange.location+redirectRange.length+1];
-                redirectUrl = [[reqUrl substringToIndex:redirectRange.location] stringByAppendingString:[NSString stringWithFormat:@"redirect_url=%@://",CompanyFirstDomainByWeChat]];
-            }else {
-                redirectUrl = [reqUrl stringByAppendingString:[NSString stringWithFormat:@"&redirect_url=%@://",CompanyFirstDomainByWeChat]];
-            }
+    if ([_urlStr containsString:@"haodf.com"]) {
+        _attachUrl = @"www.haodf.com";
+    }
+  
 
-            NSMutableURLRequest *newRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:redirectUrl] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
-            newRequest.allHTTPHeaderFields = navigationAction.request.allHTTPHeaderFields;
-            newRequest.URL = [NSURL URLWithString:redirectUrl];
-            [webView loadRequest:newRequest];
-            return;
-        }
+    if ([reqUrl hasPrefix:@"https://wx.tenpay.com/cgi-bin/mmpayweb-bin/checkmweb"] && ![reqUrl hasSuffix:[NSString stringWithFormat:@"redirect_url=%@://",CompanyFirstDomainByWeChat]]) {
+         decisionHandler(WKNavigationActionPolicyCancel);
+
+         NSString *redirectUrl = nil;
+         if ([reqUrl containsString:@"redirect_url="]) {
+             NSRange redirectRange = [reqUrl rangeOfString:@"redirect_url"];
+             endPayRedirectURL =  [reqUrl substringFromIndex:redirectRange.location+redirectRange.length+1];
+             redirectUrl = [[reqUrl substringToIndex:redirectRange.location] stringByAppendingString:[NSString stringWithFormat:@"redirect_url=%@://",_attachUrl]];
+         }else {
+             redirectUrl = [reqUrl stringByAppendingString:[NSString stringWithFormat:@"&redirect_url=%@://",_attachUrl]];
+         }
+
+         NSMutableURLRequest *newRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:redirectUrl] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
+         newRequest.allHTTPHeaderFields = navigationAction.request.allHTTPHeaderFields;
+         newRequest.URL = [NSURL URLWithString:redirectUrl];
+         [webView loadRequest:newRequest];
+         return;
+     }
 
   if (!self.hasDartNavigationDelegate) {
     decisionHandler(WKNavigationActionPolicyAllow);
